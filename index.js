78abacb613e22bf15d8782eb081a1e2ef63064e7
Max Metral <max@pyralis.com>
2017-04-07 15:38:03 -0400

    Support browser/webpack usage via creative preset construction

--- index.js
+++ index.js
@@ -1,25 +1,45 @@
 /* global module */
-module.exports = {
-  sourceMaps: "inline",
-  retainLines: true,    
+const isWebpack = !!process.env.WEBPACK_BROWSER_TARGET;
+const env = process.env.NODE_ENV || 'development';
+
+const config = {
+  sourceMaps: 'inline',
   presets: [
-    ["env", {
-      "targets": {
-        "node": "current",
-      },
-      "exclude": ["transform-async-to-generator"],
+    ['env', {
+      targets: { node: 'current' },
+      exclude: ['transform-async-to-generator'],
     }],
-    "react",
+    'react'
   ],
   plugins: [
-    ["transform-object-rest-spread", { "useBuiltIns": true }],
-    ["fast-async", { spec: true }],
-    "transform-object-entries",
-    "babel-plugin-transform-class-properties"
+    ['transform-object-rest-spread', { 'useBuiltIns': true }],
+    ['fast-async', { spec: true }],
+    'transform-object-entries',
+    'babel-plugin-transform-class-properties'
   ],
-  env: {
-    test: {
-      plugins: ["istanbul"]
-    }
-  }
 };
+
+if (isWebpack) {
+  config.presets[0][1] = {
+    targets: {
+      browsers: process.env.WEBPACK_BROWSER_TARGET,
+    },
+    modules: false,
+  };
+  if (env === 'development') {
+    config.plugins.push('react-hot-loader/babel');
+  }
+  if (env === 'production') {
+    config.presets.push('react-optimize');
+  }
+} else {
+  config.plugins.push('css-modules-transform');
+  if (env !== 'production') {
+    config.retainLines = true;
+  }
+  if (env === 'test') {
+    config.plugins.unshift('istanbul');
+  }
+}
+
+module.exports = config;
