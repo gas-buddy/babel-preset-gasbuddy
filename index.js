76d62b02e44dcf4ee7475b06a7867f6cd4c29b47
Max Metral <max@pyralis.com>
2018-12-11 16:51:29 -0500

    Pull WEBPACK_BROWSER_TARGET from env rather than upstream

--- index.js
+++ index.js
@@ -1,17 +1,8 @@
 /* global module */
-/**
- * NOTE: This may have to change in babel@7. To wit:
- * loganfsmyth [3:20 PM]
- * the point is, a preset function in 6.x is executed every time a file is compiled, in 7.x
- * we will try to reexecute them less often, so if you depend on potentially mutable state,
- * you'll have to give babel a way to know when that state changes. Using `env` will do that
- * automatically, but if you check a global yourself you'll need to handle that, once 7.x
- * lands anyway
- *
- * Will need to add something like: api.cache(() => process.env.WEBPACK_BROWSER_TARGET || '')
- */
 module.exports = function (api, options) {
   api.assertVersion(7);
+  const browserTarget = process.env.WEBPACK_BROWSER_TARGET || '> 2% in US';
+  api.cache.using(() =>  browserTarget || process.env.NODE_ENV || 'development');
 
   const isWebpack = api.env('webpack');
   // A react module is meant to be consumed by a higher level react project,
@@ -48,7 +39,7 @@ module.exports = function (api, options) {
   }
   if (isWebpack || isReactModule) {
     Object.assign(config.presets[0][1], {
-      targets: options.browsers || '> 2% in US',
+      targets: browserTarget,
       modules: false,
       useBuiltIns: 'usage',
     });
